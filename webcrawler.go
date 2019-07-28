246dfeca4fe0574a506bb71a0fc2befab4aaafe5
Dursun KATAR <45147011+dursunkatar@users.noreply.github.com>
2019-07-29 01:39:30 +0300

    Update webcrawler.go

--- webcrawler.go
+++ webcrawler.go
@@ -7,6 +7,26 @@ import (
 	"golang.org/x/net/html"
 )
 
+func main() {
+	url := "https://www.google.com/"
+
+	urls := make(chan string)
+	finish := make(chan bool)
+	go crawl(url, urls, finish)
+
+	for {
+		select {
+		case bu := <-urls:
+			fmt.Println(bu)
+		case <-finish:
+			fmt.Println("Bitti")
+			break
+		}
+	}
+
+	close(urls)
+}
+
 func crawl(url string, urls chan string, finish chan bool) {
 	res, _ := http.Get(url)
 	defer res.Body.Close()
@@ -28,24 +48,4 @@ func crawl(url string, urls chan string, finish chan bool) {
 		}
 	}
 	finish <- true
-}
-
-func main() {
-	url := "https://www.google.com/"
-
-	urls := make(chan string)
-	finish := make(chan bool)
-	go crawl(url, urls, finish)
-
-	for {
-		select {
-		case bu := <-urls:
-			fmt.Println(bu)
-		case <-finish:
-			fmt.Println("Bitti")
-			break
-		}
-	}
-
-	close(urls)
 }
